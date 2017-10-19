793fad369dfd9240f5f2383f1c422966c12836c7
xinshangshangxin <xinshangshangxin@gmail.com>
2017-10-19 10:17:22 +0800

    feat: 去除非文件夹检测

--- commit-file-jshint/index.js
+++ commit-file-jshint/index.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const fs = require('fs');
 const Promise = require('bluebird');
 const { execAsync } = require('../Util/index');
 
 const PROJECT_PATH = process.cwd();
 let ROOT_PATH;
 let headLength = process.argv[2] || '1';
+let showDeployProject = !!process.argv[3];
 
 execAsync(`cd ${PROJECT_PATH} && git rev-parse --show-toplevel`)
   .then((str) => {
@@ -17,7 +19,12 @@ execAsync(`cd ${PROJECT_PATH} && git rev-parse --show-toplevel`)
   .then((str) => {
     return getModifiedProjectPath(ROOT_PATH, str);
   })
-  .then(runJshint)
+  .then((arr) => {
+    if (showDeployProject) {
+      return null;
+    }
+    return runJshint(arr);
+  })
   .catch((e) => {
     console.warn(e);
   });
@@ -47,9 +54,32 @@ function getModifiedProjectPath(rootPath, str) {
   let result = getModifiedProject(str);
   console.info('ModifiedProject: ', result);
 
-  return Object.keys(result)
-    .map((project) => {
+  return Promise
+    .map(Object.keys(result), (project) => {
       return path.join(rootPath, project);
+    })
+    .filter((filePath) => {
+      return new Promise(function (resolve, reject) {
+        fs.stat(filePath, function (err, stats) {
+          if (err) {
+            reject(err);
+            return null;
+          }
+
+          if (stats.isDirectory()) {
+            return resolve(true);
+          }
+          return resolve(false);
+        });
+      });
+    })
+    .then(function (data) {
+      console.info('deploy project: ');
+      console.info(data.map((filePath) => {
+        return path.basename(filePath);
+      }).join(','));
+
+      return data;
     });
 }
 
